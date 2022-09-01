9c22fa370cb3e30cd573fa2c0be7ed49bc1ebbd1
Aleksey Trofimov <funkindy@gmail.com>
2022-09-01 16:37:31 +0500

    Fix: make matches saveable from admin

--- ranker/core/admin.py
+++ ranker/core/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.utils.translation import gettext_lazy as _
@@ -9,4 +10,22 @@ admin.site.site_header = _('Ranker Content Management')
 admin.site.index_title = _('Content Management')
 
 admin.site.unregister(Group)
-admin.site.register([Player, Event, Match])
+admin.site.register([Player, Event])
+
+
+class MatchForm(forms.ModelForm):
+    class Meta:
+        model = Match
+        fields = "__all__"
+
+
+class MatchAdmin(admin.ModelAdmin):
+    form = MatchForm
+    readonly_fields = [
+        'winner_rating',
+        'loser_rating',
+        'delta',
+    ]
+
+
+admin.site.register(Match, MatchAdmin)
